1c6ab0ecd92fe600793b4d849ae2f77fb74b9fb3
Andres821340 <andres.saltos@istvidanueva.edu.ec>
2025-12-13 00:20:55 -0500

    feat: tests xunit

--- tests/LinqAdvancedLab.Tests/ProductTests.cs
+++ tests/LinqAdvancedLab.Tests/ProductTests.cs
@@ -1,11 +1,32 @@
-﻿namespace LinqAdvancedLab.Tests
+﻿using Xunit;
+using Microsoft.EntityFrameworkCore;
+using LinqAdvancedLab.Data;
+using LinqAdvancedLab.Domain.Entities;
+using System.Linq;
+
+namespace LinqAdvancedLab.Tests
 {
-    public class UnitTest1
+    public class ProductTests
     {
         [Fact]
-        public void Test1()
+        public void AgregarProducto_DebeGuardar()
         {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
 
+            using (var ctx = new AppDbContext())
+            {
+                // Truco: EnsureCreated usará InMemory aquí
+                ctx.Database.EnsureCreated();
+                ctx.Products.Add(new Product { Name = "Test", Price = 10 });
+                ctx.SaveChanges();
+            }
+
+            using (var ctx = new AppDbContext())
+            {
+                Assert.Equal(1, ctx.Products.Count(p => p.Name == "Test"));
+            }
         }
     }
 }

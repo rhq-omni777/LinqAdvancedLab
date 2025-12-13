f3b5b274a49780b3f67dd6655025768fb665b1ca
Carlos Huilca <carlos.r.huilca07@gmail.com>
2025-12-13 00:02:01 -0500

    feat: specification pattern

---
+++ src/LinqAdvancedLab.Data/Repositories/Repository.cs
@@ -0,0 +1,17 @@
+﻿using LinqAdvancedLab.Domain.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace LinqAdvancedLab.Data.Repositories
+{
+    public class Repository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        public Repository(AppDbContext context) => _context = context;
+
+        public IEnumerable<T> Find(ISpecification<T> spec)
+        {
+            return _context.Set<T>().Where(spec.Criteria).ToList();
+        }
+    }
+}
